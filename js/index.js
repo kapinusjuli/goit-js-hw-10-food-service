99bbfedc26a5a71f0ade1da36a7e102246dae9d2
Капинус Юлия <kapinusjuli55@gmail.com>
2020-11-01 21:34:20 +0200

    hw10

--- js/index.js
+++ js/index.js
@@ -15,7 +15,58 @@ function createMenuMarkup (menu) {
     return menu.map(menuCardTpl).join('');
 }
 
-// console.log (menuContainer);
-// console.log(menuMarkup);
 
 menuContainer.insertAdjacentHTML('afterbegin',menuMarkup);
+
+// Переключатель тем
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+
+  
+const bodyColor = document.querySelector('body');
+
+const themeChange = document.querySelector('.theme-switch__toggle')
+
+themeChange.addEventListener('change', clickHandler);
+themeChange.addEventListener('change', setLocalStorage);
+document.addEventListener('DOMContentLoaded', getThemeFromLocalStorage);
+
+
+function clickHandler(evt) {
+if (themeChange.checked) {
+    setDarkTheme()
+} else {
+    setLightTheme()
+}
+}
+
+function setLocalStorage(evt) {
+    if (themeChange.checked) {
+        localStorage.setItem('theme', Theme.DARK);
+    } else {
+        localStorage.removeItem('theme');
+        localStorage.setItem('theme', Theme.LIGHT);
+    }
+}
+
+function getThemeFromLocalStorage() {
+    const themeInLocalStrg = localStorage.getItem('theme');
+    if (themeInLocalStrg === Theme.DARK) {
+        bodyColor.classList.add(Theme.DARK);
+        themeChange.checked = true;
+    }
+}
+
+function setDarkTheme() {
+    bodyColor.classList.add(Theme.DARK);
+    bodyColor.classList.remove(Theme.LIGHT);
+}
+
+function setLightTheme() {
+    bodyColor.classList.add(Theme.LIGHT);
+    bodyColor.classList.remove(Theme.DARK);
+}
