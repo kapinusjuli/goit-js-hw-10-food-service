bb7f8efebc9773afb85ed9496967b0ce732b7600
Капинус Юлия <kapinusjuli55@gmail.com>
2020-11-01 06:06:16 +0200

    *hw

--- js/index.js
+++ js/index.js
@@ -2,5 +2,18 @@ import menuCardTpl from '../src/templates/menu-card.hbs';
 import menu from '../menu.json';
 
 
-console.log (menuCardTpl);
-console.log (menu);
+// console.log (menuCardTpl(menu[0]));
+// console.log (menu);
+
+const menuContainer = document.querySelector('.js-menu');
+const menuMarkup = JSON.stringify(createMenuMarkup (menu));
+
+
+console.log (menuContainer);
+
+function createMenuMarkup (menu) {
+    return menu.map(menuCardTpl).join('');
+}
+
+
+menuContainer.insertAdjacentElement('beforeend', menuMarkup);
